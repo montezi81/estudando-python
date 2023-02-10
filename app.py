a3c46a36ec4f6f567964200ed7f60d86d39d96f2
Marcos Costa <mvmontezi@gmail.com>
2023-02-10 12:16:02 -0300

    Estudando o Python

---
+++ app.py
@@ -0,0 +1,24 @@
+# Treinamento de Python
+# Variáveis
+# Tipos: Números, Booleanos e Strings
+
+# Números
+velocidade_internet = 10
+print(velocidade_internet)
+
+#Booleanos
+esta_aberta = True
+print(esta_aberta)
+
+# String
+slogan_A = 'Feito é melhor que perfeito !'
+slogan_B = "Feito é melhor que perfeito !"
+print(slogan_A, slogan_B)
+
+#Unpacking
+a,b,c,d = 1,2,3,4
+print(a,b,c,d)
+print(type(a))
+print(type(slogan_A))
+
+
---
+++ tiposdevariaveis.py
@@ -0,0 +1,10 @@
+variavel_1 = 25.87          # float
+variavel_2 = True           # bool
+variavel_3 = 'Bom dia !'    # str
+variavel_4 = 15             # int
+
+print(type(variavel_1))
+print(type(variavel_2))
+print(type(variavel_3))
+print(type(variavel_4))
+
