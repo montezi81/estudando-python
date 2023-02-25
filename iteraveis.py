e09a364b7fab7224100be99c1e6edc0ed4c5ae4d
montezi81 <mvmontezi@gmail.com>
2023-02-25 18:06:40 -0300

    Iteraveis

---
+++ iteraveis.py
@@ -0,0 +1,23 @@
+# Iteráveis.
+# passar por dentro do for diversas vezes
+
+# Iterável de número inteiro
+for item in range (10):
+    print(item)
+
+# Iterável de string
+for letra in 'programação':
+    print(letra)
+
+# Iterável da lista
+for item1 in [2,3,4,5]:
+    print(item1)
+
+for nomes_diversos in ['Marcos', 'Vinicius', 'Montezi', 10]:
+    print(nomes_diversos)
+
+nomes = ['1', '2', '3', '4', '5']
+for nome in nomes:
+    print(nome)
+
+
